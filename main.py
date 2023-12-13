7940f937f3b3eea9e87288cdedd6fa7f153bc1ae
kamila-lim <56233217+kamila-lim@users.noreply.github.com>
2023-12-13 23:39:19 +0300

    Update main.py
    исправление сохранения времени, перевод вывода данных на англ яз, исправления сохранения данных в базу weather.db

--- main.py
+++ main.py
@@ -6,61 +6,68 @@ from datetime import datetime, timezone, timedelta
 api_key = "2541ab2df295ab1935fab364a53109d3"
 
 def print_weather_info(location: str, data_from_file: dict):
+
     utc_timestamp = data_from_file["dt"]
+
     offset = data_from_file["timezone"]
 
     tz = timezone(timedelta(seconds=offset))
     result_time = datetime.fromtimestamp(utc_timestamp, tz)
+
     formatted_time = result_time
 
     print("текущее время:", formatted_time)
     print("название города:", location)
     print("погодные условия:", data_from_file["weather"][0]["description"])
-    print("текущая температура:", data_from_file["main"]["temp"], "градусов по цельсию")
-    print("ощущается как:", data_from_file["main"]["feels_like"], "градусов по цельсию")
-    print("скорость ветра:", data_from_file["wind"]["speed"], "м/c")
+    print("текущая температура:", data_from_file["main"]["temp"], "degrees Celsius")
+    print("ощущается как:", data_from_file["main"]["feels_like"], "degrees Celsius")
+    print("скорость ветра:", data_from_file["wind"]["speed"], "m/c")
 
 def get_weather_by_city(city: str):
+    """
+    Получение информации о погоде по названию города.
+
+    Аргумент:
+    city - название города.
+    """
+
     try:
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&"
         response = requests.get(url)
-        response.raise_for_status()
+        response.raise_for_status()  
         data_from_file = response.json()
 
         print_weather_info(city, data_from_file)
-        save_to_database((datetime.now(), city, data_from_file["weather"][0]["description"],
-                          data_from_file["main"]["temp"], data_from_file["main"]["feels_like"],
-                          data_from_file["wind"]["speed"]))
+        save_to_database((datetime.now(), city, data_from_file["weather"][0]["description"], data_from_file["main"]["temp"], data_from_file["main"]["feels_like"], data_from_file["wind"]["speed"]))
     except requests.exceptions.HTTPError as err:
-        print("http error occurred, попробуйте снова!")
+        print("Такого города не существует, попробуйте снова!")
     except requests.exceptions.RequestException as err:
-        print("request exception occurred, попробуйте снова!")
+        print("Проблемы с соединением, попробуйте позже!")
 
 def get_weather_by_location():
+
     try:
         location = geocoder.ip("me")
 
         if location:
             latitude, longitude = location.latlng
-            url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric&lang=ru"
+            url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric&"
             response = requests.get(url)
-            response.raise_for_status()
+            response.raise_for_status()  
             data_from_file = response.json()
-
+           
             print_weather_info(location.address, data_from_file)
-            save_to_database((datetime.now(), location.address, data_from_file["weather"][0]["description"],
-                              data_from_file["main"]["temp"], data_from_file["main"]["feels_like"],
-                              data_from_file["wind"]["speed"]))
+            save_to_database((datetime.now(), location.address, data_from_file["weather"][0]["description"], data_from_file["main"]["temp"], data_from_file["main"]["feels_like"], data_from_file["wind"]["speed"]))
         else:
-            print("не удалось определить текущее местоположение")
+            print("Не удалось определить текущее местоположение")
     except requests.exceptions.HTTPError as err:
-        print("http error occurred, попробуйте снова!")
+        print("Такого города не существует, попробуйте снова!")
     except requests.exceptions.RequestException as err:
-        print("request exception occurred, попробуйте снова!")
+        print("Проблемы с соединением, попробуйте позже!")
 
 def save_to_database(data: tuple):
     try:
-        connection = sqlite3.connect("weather.txt")
+        connection = sqlite3.connect("weather.db")
         cursor = connection.cursor()
 
         cursor.execute('''create table if not exists weather_requests
@@ -72,38 +79,38 @@ def save_to_database(data: tuple):
                           feeling float,
                           speed float)''')
 
-        formatted_time = datetime.now().strftime('%y-%m-%d %h:%m:%s')
-        cursor.execute(
-            "insert into weather_requests (timestamp, city, weather_condition, temperature, feeling, speed) values (?, ?, ?, ?, ?, ?)",
-            (formatted_time,) + data[1:])
+        formatted_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        cursor.execute("INSERT INTO weather_requests (timestamp, city, weather_condition, temperature, feeling, speed) VALUES (?, ?, ?, ?, ?, ?)", (formatted_time,) + data[1:])
         connection.commit()
         connection.close()
     except sqlite3.Error as error:
         print("ошибка при работе с базой данных, попробуйте снова!")
 
 def print_history(n: str):
+
     try:
         n = int(n)
         if n < 0:
-            print("введите значение n > 0")
+            print("Введите значение n > 0")
         else:
-            connection = sqlite3.connect("weather.txt")
+            connection = sqlite3.connect("weather.db")
             cursor = connection.cursor()
-            cursor.execute(f"select * from weather_requests order by timestamp desc limit {n}")
+            cursor.execute(f"SELECT * FROM weather_requests ORDER BY timestamp DESC LIMIT {n}")
             results = cursor.fetchall()
             connection.close()
 
-            print("последние", n, "запросов:")
+            print("Последние", n, "запросов:")
             print("=" * 20)
             for result in results:
                 print("время запроса:", result[1])
                 print("название города:", result[2])
                 print("погодные условия:", result[3])
-                print("температура:", result[4], "градусов по цельсию")
-                print("ощущение:", result[5], "градусов по цельсию")
-                print("скорость ветра:", result[6], "м/c")
+                print("температура:", result[4], "degrees Celsius")
+                print("ощущение:", result[5], "degrees Celsius")
+                print("скорость ветра:", result[6], "m/c")
                 print("=" * 20)
-
+            
     except ValueError:
         print("введите целое число для 'history'.")
     except sqlite3.OperationalError:
@@ -139,3 +146,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
